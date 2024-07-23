424d0dd26b4fb623ddff7880f61f543339533a23
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-07-23 10:55:59 +0100

    Deleted array section of submission script

--- env_build.sh
+++ env_build.sh
@@ -3,7 +3,6 @@
 #SBATCH --job-name=env build # the name for the cluster scheduler
 #SBATCH --time=00:10:00 # Maximum allowed runtime per iteration
 #SBATCH --mem-per-cpu=7G
-#SBATCH --array=1-10 # the number of iterations
 #SBATCH --output=logfiles/example_%A_%a.out # the name of the output files
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=david.hemprich-bennett@biology.ox.ac.uk
