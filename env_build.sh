70f88190b7223fb2cc097b9f738e662e0bc60aad
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-07-23 10:55:25 +0100

    Converted to slurm script

--- env_build.sh
+++ env_build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+#SBATCH --job-name=env build # the name for the cluster scheduler
+#SBATCH --time=00:10:00 # Maximum allowed runtime per iteration
+#SBATCH --mem-per-cpu=7G
+#SBATCH --array=1-10 # the number of iterations
+#SBATCH --output=logfiles/example_%A_%a.out # the name of the output files
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=david.hemprich-bennett@biology.ox.ac.uk
+
+
 module load Anaconda3
 
 export CONPREFIX=$DATA/conda_envs/temp
 conda create --prefix $CONPREFIX
 source activate $CONPREFIX
 conda env update --file environment.yml --prune
+
+mkdir logfiles
